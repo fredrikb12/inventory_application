445ac99977a7c867b49fa9bb9548b1b7515dcfea
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-01 13:16:52 +0200

    Create models

---
+++ models/category.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 40 },
+  description: { type: String, required: true, minlength: 10, maxlength: 100 },
+});
+
+CategorySchema.virtual("url").get(() => "/shop/category/" + this._id);
+
+module.exports = mongoose.model("Category", CategorySchema);
---
+++ models/item.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 40 },
+  description: { type: String, required: true, minlength: 10, maxlength: 100 },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  price: { type: Number, required: true },
+  in_stock: { type: Number, required: true },
+});
+
+ItemSchema.virtual("url").get(() => "/shop/item/" + this._id);
+
+module.exports = mongoose.model("Item", ItemSchema);
